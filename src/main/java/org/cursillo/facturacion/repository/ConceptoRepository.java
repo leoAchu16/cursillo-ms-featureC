1c60b6987439fe6b25c5bd643337be4908f2cbc7
DavidCristaldo <david.cristaldo@fiuni.edu.py>
2026-09-24 14:09:22 -0300

    Archivo ConceptoRepository.java

--- src/main/java/org/cursillo/facturacion/repository/ConceptoRepository.java
+++ src/main/java/org/cursillo/facturacion/repository/ConceptoRepository.java
@@ -1,4 +1,19 @@
 package org.cursillo.facturacion.repository;
 
-public interface ConceptoRepository {
+import org.cursillo.commons.entities.Conceptos.Concepto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import java.util.Optional;
+
+@Repository
+public interface ConceptoRepository extends JpaRepository<Concepto, Integer> {
+
+    @Query("SELECT c FROM Concepto c WHERE (:nombre IS NULL OR LOWER(c.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND c.activo = true")
+    Page<Concepto> search(@Param("nombre") String nombre, Pageable pageable);
+    Page<Concepto> findAllByActivoTrue(Pageable pageable);
+    Optional<Concepto> findByIdConceptoAndActivoTrue(Integer idConcepto);
 }
