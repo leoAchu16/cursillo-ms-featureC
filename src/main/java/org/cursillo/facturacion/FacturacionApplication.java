b85478c86469544e79e7fc644976a7fb55bdc1ff
DavidCristaldo <david.cristaldo@fiuni.edu.py>
2026-09-24 14:01:23 -0300

    archivo FacturaApplication.java

--- src/main/java/org/cursillo/facturacion/FacturacionApplication.java
+++ src/main/java/org/cursillo/facturacion/FacturacionApplication.java
@@ -1,4 +1,16 @@
 package org.cursillo.facturacion;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = "org.cursillo.commons.entities")
+@EnableJpaRepositories(basePackages = "org.cursillo.facturacion.repository")
 public class FacturacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturacionApplication.class, args);
+    }
 }
