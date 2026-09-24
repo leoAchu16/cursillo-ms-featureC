f19ac3212fbfc34cdabd8795c9581986f3f667f0
DavidCristaldo <david.cristaldo@fiuni.edu.py>
2026-09-24 14:10:10 -0300

    Archivo DetallesFacturaRepository

--- src/main/java/org/cursillo/facturacion/repository/DetallesFacturaRepository.java
+++ src/main/java/org/cursillo/facturacion/repository/DetallesFacturaRepository.java
@@ -1,4 +1,15 @@
 package org.cursillo.facturacion.repository;
 
-public interface DetallesFacturaRepository {
+import org.cursillo.commons.entities.Factura.DetallesFactura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@Repository
+public interface DetallesFacturaRepository extends JpaRepository<DetallesFactura, Integer> {
+
+    Page<DetallesFactura> findAllByActivoTrue(Pageable pageable);
+    Optional<DetallesFactura> findByIdDetalleFacturaAndActivoTrue(Integer idDetalleFactura);
 }
