a6f0632aadaaa2fb11f1be08df9bfd9758a06814
DavidCristaldo <david.cristaldo@fiuni.edu.py>
2026-09-24 14:04:08 -0300

    Archivo ErrorResponse.java

--- src/main/java/org/cursillo/facturacion/exception/ErrorResponse.java
+++ src/main/java/org/cursillo/facturacion/exception/ErrorResponse.java
@@ -1,4 +1,23 @@
+// ErrorResponse.java
 package org.cursillo.facturacion.exception;
 
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+
+    public ErrorResponse(int status, String error, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public int getStatus() { return status; }
+    public String getError() { return error; }
+    public String getMessage() { return message; }
 }
